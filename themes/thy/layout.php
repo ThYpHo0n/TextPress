9996f4391360bed4869c6a0b1e4b327e42ab0c0a
Niklas Grebe <niklas.grebe@innogames.de>
2013-11-22 19:28:55 +0100

    * Added tracking

--- themes/thy/layout.php
+++ themes/thy/layout.php
@@ -22,24 +22,15 @@
     <link href="<?php echo $global['assets.prefix'];?>/themes/textpress/assets/css/bootstrap-responsive.min.css" rel="stylesheet">
     <link href="<?php echo $global['assets.prefix'];?>/themes/textpress/assets/css/main.css" rel="stylesheet">
 
-    <!-- Le fav and touch icons -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-45962279-1', 'thyphoon.org');
+      ga('send', 'pageview');
+    </script>
 
-    <?php if($global['google.analytics']){?>
-    <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', '<?php echo $global['google.analytics']; ?>']);
-      _gaq.push(['_trackPageview']);
-
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-      $(function(){
-        $('.dropdown-toggle').dropdown()
-      })
-  </script>
-  <?php }?>
   </head>
 
   <body>
